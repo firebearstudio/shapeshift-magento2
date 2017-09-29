e2c2bffd56e702216aa1f32464ea9ce1e6836a73
Artsiom Dziadziula <ded2.94@tut.by>
2017-09-29 10:37:34 +0000

    Initialize ShapeShift module and payment method

---
+++ Model/Methods/Shapeshift.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Firebear\ShapeShift\Model\Methods;
+
+use Magento\Payment\Model\Method\AbstractMethod;
+
+class Shapeshift extends AbstractMethod
+{
+
+}
---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Firebear_ShapeShift',
+    __DIR__
+);
